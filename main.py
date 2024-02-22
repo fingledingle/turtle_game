65884c49c2f8d65181fa4a8d7a8a224560143165
mmmmmm <elenai@riseup.net>
2024-02-22 18:16:38 +0800

    change blablabla

---
+++ main.py
@@ -0,0 +1,42 @@
+import turtle
+import turtle as t
+from turtle import Turtle, Screen
+import random
+
+is_race_on = False
+screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make Your Bet", prompt="Which turtle will win the race? Enter a color: ")
+
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+all_turtles = []
+
+
+height_value = 150
+for turtle_color in colors:
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(turtle_color)
+    new_turtle.penup()
+    new_turtle.goto(x = -230, y = height_value)
+    height_value -= 50
+    all_turtles.append(new_turtle)
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for turtle in all_turtles:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You have won! {winning_color} is the winner!")
+            else:
+                print(f"You have lost! {winning_color} is the winner")
+        random_distance = random.randint(0, 10)
+        turtle.forward(random_distance)
+
+    
+
+
+screen.exitonclick()
